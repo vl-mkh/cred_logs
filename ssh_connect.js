6d97fd06ac4986ff62db690f68b2713f2e8c5ad7
vladm <vladmelekh@gmail.com>
2018-03-11 19:51:03 +0300

    html start

--- ssh_connect.js
+++ ssh_connect.js
@@ -1,32 +1,45 @@
 var fs, node_ssh, ssh;
-
+var log_path_remote = './html/var/log/credo_microservices.log';
+var log_path_local = 'C:\\Users\\vladm\\Documents\\FrontProjects\\credo_logs\\logs\\credo_microservices.log'
 
 node_ssh = require('node-ssh');
 ssh = new node_ssh();
 fs = require('fs');
 
+var re_log = /(\{"method".*\}),"request_params"/gi;
+var re_time = /^\[([\W\d]+)\]/gi;
+
 ssh.connect({
     host: 'credo4.fortyfour.com',
     username: 'www-data',
     password: 'c0mm3rce'
-  }).then(function() {
-      ssh.getFile('C:\\Users\\vladm\\Documents\\FrontProjects\\credo_logs\\logs\\credo_microservices.log', './html/var/log/credo_microservices.log').then(function(Contents) {
-        console.log("The File's contents were successfully downloaded")
-      }, function(error) {
-        console.log("Something's wrong")
-        console.log(error)
-      })
-  });
+    })
+    .then(function() {
+        ssh.getFile(log_path_local, log_path_remote)
+          .then(function() {
+                  console.log("The File's contents were successfully downloaded");
+                }, 
+                function(error) {
+                  console.log("Something's wrong")
+                  console.log(error)
+                })
+              }
+        );
 
-var array = fs.readFile('C:\\Users\\vladm\\Documents\\FrontProjects\\credo_logs\\logs\\credo_microservices.log', 'utf8', (err, data) => {
-    if (err) {
-      throw err;
-    }
-    content = data;
-    parseFile();
-  });
+var credo_logs = fs.readFileSync(log_path_local, 'utf8').split('\n').slice(-5);
 
-function parseFile() {
-  data = content.split('\n').slice(-2)
-  console.log(data)
+function parse_logs(array) {
+  var parsed_logs = [];
+  for (i = 0; i < array.length; i++) {
+      if (array[i] == undefined || array[i].length == 0) {
+          continue;
+      }
+      log = array[i].match(re_log);
+      time = array[i].match(re_time);
+      log_time = [time[0], log[0]];
+      parsed_logs.push(log_time);     
   }
+  return parsed_logs;
+}
+
+console.log(parse_logs(credo_logs));
